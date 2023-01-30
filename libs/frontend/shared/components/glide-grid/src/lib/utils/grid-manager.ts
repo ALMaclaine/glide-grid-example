bb51fe7779f8bcf22beec20540f3d813456e3c66
Alessandro Maclaine <almaclaine@gmail.com>
2023-01-29 23:20:48 -0600

    remove row-cache

--- libs/frontend/shared/components/glide-grid/src/lib/utils/grid-manager.ts
+++ libs/frontend/shared/components/glide-grid/src/lib/utils/grid-manager.ts
@@ -1,10 +1,13 @@
 import { Columns } from './columns';
-import { RowsManager } from '../rows-manager';
 import { Cell, IdRow, WrappedGridColumn } from '../types/grid';
 import { StringKeys } from '../types/general';
 import { CellCache } from './caches/cell-cache';
 import { GridCell, Item } from '@glideapps/glide-data-grid';
 import { SortMap } from './sort/sort-map';
+import { TableSorter } from './sort/table-sorter';
+import { SortStateMachine } from './sort/sort-state-machine';
+import { STATE_HISTORY_STEPS } from '../constants';
+import { uuid } from './general';
 
 type GridManagerProps<T> = {
   columns: WrappedGridColumn<T>[];
@@ -12,15 +15,31 @@ type GridManagerProps<T> = {
   hiddenColumns?: StringKeys<T>[];
 };
 
+const addIdsToRows = <T>(rows: T[]): IdRow<T>[] => {
+  // mutate directly to avoid performance issues on large tables
+  for (const row of rows) {
+    const changeType = row as IdRow<T>;
+    changeType.rowUuid = uuid();
+  }
+  return rows as IdRow<T>[];
+};
+
 class GridManager<T> {
   private readonly _columns: Columns<T>;
-  private readonly _rowManager: RowsManager<T>;
   readonly cellCache: CellCache<T>;
+  private readonly rows: IdRow<T>[];
+
+  readonly stateMachine: SortStateMachine<T> = new SortStateMachine<T>();
+  readonly sorter: TableSorter<T>;
 
   constructor({ columns, data, hiddenColumns }: GridManagerProps<T>) {
     this._columns = new Columns<T>({ columns, hiddenColumns });
     const sortMap = new SortMap({ columns });
-    this._rowManager = new RowsManager<T>(data, sortMap);
+    this.rows = addIdsToRows(data);
+    this.sorter = new TableSorter({
+      sortMap,
+    });
+    this.sorter.addData(this.rows);
     this.cellCache = this.setupCellCache(columns);
   }
 
@@ -36,7 +55,7 @@ class GridManager<T> {
   private setupCellCache(columns: WrappedGridColumn<T>[]) {
     const cellCache = new CellCache<T>();
     for (let row = 0; row < this.length; row++) {
-      const item = this._rowManager.getRowByIndex(row);
+      const item = this.rows[row];
       const { rowUuid } = item;
       for (let col = 0; col < columns.length; col++) {
         const { columnUuid } = columns[col];
@@ -48,7 +67,7 @@ class GridManager<T> {
   }
 
   itemToCell([col, row]: Item): GridCell {
-    const { rowUuid } = this._rowManager.sorted[row];
+    const { rowUuid } = this.sorter.sorted[row];
     const translatedCol = this._columns.getTranslation(col);
     return this.cellCache.get(rowUuid, translatedCol) as GridCell;
   }
@@ -66,15 +85,16 @@ class GridManager<T> {
   }
 
   get length() {
-    return this._rowManager.length;
+    return this.sorter.length;
   }
 
   getHistory(steps: number) {
-    return this._rowManager.stateMachine.getHistory(steps);
+    return this.stateMachine.getHistory(steps);
   }
 
   nextSortKey(key: StringKeys<T>) {
-    this._rowManager.nextSortKey(key);
+    const stateHistory = this.stateMachine.nextValue(key, STATE_HISTORY_STEPS);
+    return this.sorter.stateSort(stateHistory);
   }
 }
 
